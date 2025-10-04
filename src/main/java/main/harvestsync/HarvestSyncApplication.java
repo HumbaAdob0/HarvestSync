7a19c7dbfeff065dbc9e1320a3119cd213efd827
Jornheym P. Remegia <remegiajonrheym0911@gmail.com>
2025-10-04 23:12:17 +0800

    initial commit

---
+++ src/main/java/main/harvestsync/HarvestSyncApplication.java
@@ -0,0 +1,13 @@
+package main.harvestsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarvestSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarvestSyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/main/harvestsync/HarvestSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package main.harvestsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarvestSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
